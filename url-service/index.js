be58122652f58b0dc411285abe674957cf14fa04
jeongjulee42 <ljj3342@naver.com>
2025-04-13 12:06:47 +0900

    .env

--- url-service/index.js
+++ url-service/index.js
@@ -1,28 +1,39 @@
 import express from 'express';
 import { nanoid } from 'nanoid';
+import { pool } from './db.js';
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 
-// POST /shorten
-app.post('/shorten', (req, res) => {
+app.post('/shorten', async (req, res) => {
     const { original_url } = req.body;
 
     if (!original_url) {
-        return res.status(400).json({ error: "original_url is required" });
+        return res.status(400).json({ error: 'original_url is required' });
     }
 
     const short_code = nanoid(8);
-    console.log(`[Shorten] ${original_url} -> ${short_code}`);
 
-    res.json({
-        short_url: `http://localhost/${short_code}`,
-        code: short_code
-    });
+    try {
+            const query = `
+                INSERT INTO urls (original_url, short_code)
+                VALUES ($1, $2)
+                RETURNING short_code
+                `;
+            const result = await pool.query(query, [original_url, short_code]);
+
+            res.json({
+                short_url: `http://localhost/${result.rows[0].short_code}`,
+                code: result.rows[0].short_code
+            });
+        } catch (err) {
+            console.error('DB Insert Error:', err);
+            res.status(500).json({ error: 'Database error' });
+        }
 });
 
 app.listen(PORT, () => {
-    console.log(`✅ URL Service is running on http://localhost:${PORT}`);
+    console.log(`✅ URL Service running on http://localhost:${PORT}`);
 });
